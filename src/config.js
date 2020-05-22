46e7d2068d4e1ca0b4cc2f7ad99713ba633befce
Chris Hjorth <contact@chrishjorth.com>
2020-05-22 11:15:56 +0200

    Added stage separation.

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const BACKEND_URL = 'https://v7qqtjkwvj.execute-api.eu-west-1.amazonaws.com/dev'
+const _BACKEND_URL = process.env.NODE_ENV === 'production' ? 'https://a8w3q11yde.execute-api.eu-west-1.amazonaws.com/prod' : 'https://v7qqtjkwvj.execute-api.eu-west-1.amazonaws.com/dev'
+
+export const BACKEND_URL = _BACKEND_URL
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,74 @@
 const webpack = require('webpack')
 const path = require('path')
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: [
-    './index.js'
-  ],
-  mode: 'development',
-  output: {
-    filename: '[name]-bundle.dev.js',
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/'
-  },
-  devServer: {
-    hot: true,
-    contentBase: path.resolve(__dirname, 'build'),
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+module.exports = (env, argv) => {
+  if (argv.mode === 'production') {
+    return {
+      context: path.resolve(__dirname, 'src'),
+      entry: [
+        './index.js'
+      ],
+      mode: 'production',
+      output: {
+        filename: '[name]-bundle.prod.js',
+        path: path.resolve(__dirname, 'build/prod'),
+        publicPath: '/'
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin()
-  ]
+      module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader'
+            }
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+          }
+        ]
+      },
+      plugins: [
+        new webpack.NamedModulesPlugin()
+      ]
+    }
+  } else {
+    return {
+      context: path.resolve(__dirname, 'src'),
+      entry: [
+        './index.js'
+      ],
+      mode: 'development',
+      output: {
+        filename: '[name]-bundle.dev.js',
+        path: path.resolve(__dirname, 'build/dev'),
+        publicPath: '/'
+      },
+      devServer: {
+        hot: true,
+        contentBase: path.resolve(__dirname, 'build/dev'),
+        publicPath: '/'
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader'
+            }
+          },
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+          }
+        ]
+      },
+      plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+      ]
+    }
+  }
 }
