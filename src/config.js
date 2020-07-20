692ff935565686bb3c1065fa8f2084d695abe61a
Chris Hjorth <contact@chrishjorth.com>
2020-07-20 16:13:47 +0200

    Development build.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const _BACKEND_URL = process.env.NODE_ENV === 'production' ? 'https://a8w3q11yde.execute-api.eu-west-1.amazonaws.com/prod' : 'https://v7qqtjkwvj.execute-api.eu-west-1.amazonaws.com/dev'
+const _BACKEND_URL = process.env.NODE_ENV === 'production' ? 'https://a8w3q11yde.execute-api.eu-west-1.amazonaws.com/prod' : 'https://ykrlxfdod7.execute-api.eu-west-1.amazonaws.com/dev'
 
 export const BACKEND_URL = _BACKEND_URL
 export const APP_PATH = process.env.NODE_ENV === 'production' ? '/admin/apps/recaptcha-spambuster' : '/admin/apps/recaptcha-spambuster-dev'
